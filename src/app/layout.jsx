db617523732af495b3f6bafadb59bb7af96daa25
eudinson <eudinson_uy29@yahoo.com>
2023-06-12 21:48:58 +0800

    redeploying app

---
+++ src/app/layout.jsx
@@ -0,0 +1,23 @@
+import { Poppins } from 'next/font/google'
+
+const font = Poppins({
+  weight: ['400', '600', '700'],
+  subsets: ['latin']
+})
+
+export const metadata = {
+  title: 'Todo App',
+  description: 'A simple todo app by Eudin',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={font.className}>
+        <main>
+          {children}
+        </main>
+      </body>
+    </html>
+  )
+}
---
+++ src/app/page.jsx
@@ -0,0 +1,199 @@
+'use client';
+import styles from '../styles/page.module.css';
+import { useState, useEffect } from "react";
+import styled from '@emotion/styled';
+import {
+  TextField,
+  Button,
+  Container,
+  Box,
+  Card,
+  TableContainer,
+  Table,
+  TableHead,
+  TableRow,
+  TableBody,
+  TableCell,
+  Paper,
+  IconButton,
+  Tooltip
+} from "@mui/material";
+import {
+  DeleteOutline,
+  EditOutlined,
+  Close,
+  Check
+} from '@mui/icons-material';
+
+const HomePage = () => {
+
+  const [inputValue, setInputValue] = useState('');
+  const [editInputValue, setEditInputValue] = useState('');
+
+  const [todoId, setTodoId] = useState(null);
+  const [todoList, setTodoList] = useState([]);
+
+  useEffect(() => {
+
+  }, [])
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const todo = {
+      id: new Date().getTime(),
+      task: inputValue
+    }
+    setTodoList([...todoList, todo]);
+    setInputValue('');
+  }
+
+  const handleInputChange = (e) => {
+    const { value } = e.target;
+    setInputValue(value);
+  }
+
+  const handleDelete = (id) => {
+    const newTodos = todoList.filter(data => data.id !== id);
+    setTodoList(newTodos);
+  }
+
+  const handleEdit = (id) => {
+    setTodoId(id)
+    const editValue = todoList.filter(data => data.id === id).map(data => data.task);
+    setEditInputValue(editValue);
+  }
+
+  const handleEditInput = (e) => {
+    const { value } = e.target;
+    setEditInputValue(value);
+  }
+
+  const handleUpdate = (id) => {
+    todoList.filter(data => data.id === id).map(data => data.task = editInputValue);
+    setTodoId(null);
+  }
+
+  const handleCancel = () => {
+    setTodoId(null);
+  }
+
+  return (
+    <Box>
+
+      <Container>
+        <Card
+          className={styles.headerCard}
+          variant='outlined'
+        >
+          <h1 className={styles.header}>Simple Todo List App</h1>
+        </Card>
+      </Container>
+
+      <Container>
+        <form action="" className={styles.form} onSubmit={handleSubmit}>
+          <TextField
+            id="standard-basic"
+            label="Type here & press enter to add"
+            variant="outlined"
+            onChange={handleInputChange}
+            value={inputValue}
+            size='small'
+            required
+            fullWidth
+          />
+
+          {/* <Button
+            variant="contained"
+            fullWidth
+            type='submit'
+            size='medium'
+            startIcon={<AddOutlined />}
+          >
+            Add Todo
+          </Button> */}
+        </form>
+      </Container>
+      <br />
+      <Container>
+        {
+          todoList.length > 0
+          &&
+          <TableContainer component={Paper}>
+            <Table aria-label="customized table">
+              <TableHead className={styles.tableHead}>
+                <TableRow>
+                  <TableCell component="th" scope="row" className={styles.tableHeadCell}>
+                    Todos
+                  </TableCell>
+                  <TableCell component="th" scope="row" className={styles.tableHeadCell}>
+                    Actions
+                  </TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {
+                  todoList.map(({ id, task }) => (
+                    <>
+                      {
+                        todoId === id
+                          ?
+                          <TableRow key={id}>
+                            <TableCell component="th" scope="row">
+                              <TextField
+                                id="standard-basic"
+                                label="Edit task"
+                                variant="standard"
+                                onChange={handleEditInput}
+                                value={editInputValue}
+                                size='small'
+                                fullWidth
+                              />
+                            </TableCell>
+                            <TableCell component="th" scope="row" width={60}>
+                              <Tooltip title="Update">
+                                <IconButton aria-label="update" size="small" color='primary' onClick={() => handleUpdate(id)}>
+                                  <Check fontSize="small" />
+                                </IconButton>
+                              </Tooltip>
+                              <Tooltip title="Cancel">
+                                <IconButton aria-label="cancel" size="small" color='error' onClick={() => handleCancel(id)}>
+                                  <Close fontSize="small" />
+                                </IconButton>
+                              </Tooltip>
+                            </TableCell>
+                          </TableRow>
+                          :
+                          <TableRow key={id}>
+                            <TableCell component="th" scope="row">
+                              {
+                                task
+                              }
+                            </TableCell>
+                            <TableCell component="th" scope="row" width={60}>
+                              <Tooltip title="Edit">
+                                <IconButton aria-label="edit" size="small" color='primary' onClick={() => handleEdit(id)}>
+                                  <EditOutlined fontSize="small" />
+                                </IconButton>
+                              </Tooltip>
+                              <Tooltip title="Delete">
+                                <IconButton aria-label="delete" size="small" color='error' onClick={() => handleDelete(id)}>
+                                  <DeleteOutline fontSize="small" />
+                                </IconButton>
+                              </Tooltip>
+                            </TableCell>
+                          </TableRow>
+                      }
+                    </>
+                  ))
+                }
+              </TableBody>
+            </Table>
+          </TableContainer>
+        }
+      </Container>
+
+    </Box>
+  );
+}
+
+export default HomePage;
